6c577d584b82799adb72c492020b78a35695c9cf
Emily Vu <emilyhvu@berkeley.edu>
2017-03-28 22:50:46 -0700

    Add files via upload

--- FixedSourceSolver.m
+++ FixedSourceSolver.m
@@ -3,12 +3,6 @@ function [anal_phi ,phi, mesh] = FixedSourceSolver(a,D,Sigma_A,S,h)
 
 mesh=[-a:h:a];
 
-%fake_mesh=[]; %unecessary for this code but would be used for non-uniform spacing
-%for i=1:length(mesh)-1 
-%    fake_mesh=[fake_mesh mesh(i)+((mesh(i+1)-mesh(i)))/2];
-%end
-%fake_mesh=[mesh(1) fake_mesh mesh(length(mesh))];
-
 A=zeros(length(mesh)-2,length(mesh)); 
 for i=1:length(mesh)-2
     A(i,i:i+2)=[-D/h^2 2*D/h^2+Sigma_A -D/h^2]; 
@@ -16,8 +10,21 @@ end
 A=A(:,2:length(mesh)-1);
 b=(ones(1,length(mesh)-2)*S)';
 
-phi=A\b; %NUMERICAL (FINITE DIFF METHOD)
-phi=[0;phi;0];
+dim=size(A,1); %Gauss Elem
+Ab=[A b];
+for i=2:dim %Forward Sub
+   Ab(i-1,:)=Ab(i-1,:)./Ab(i-1,i-1); %Normalize
+   Ab(i,:)=Ab(i,:)-Ab(i-1,:).*Ab(i,i-1);
+end
+A=Ab(1:dim,1:dim);
+b=Ab(:,dim+1);
+
+phi=zeros(dim,1);
+phi(dim)=b(dim)/A(dim,dim);
+for i=dim-1:-1:1 %Backward Sub
+    phi(i)=b(i)-A(i,i+1)*phi(i+1);
+end
+phi=[0;phi;0]; %NUMERICAL (FINITE DIFF METHOD)
 
 plot(mesh,phi,'r+')
 title('Fixed-Source Diffusion Equation')
